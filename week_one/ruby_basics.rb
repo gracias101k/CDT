cef269711cae7bd6eb2e46e29273d4d5fea3ed44
gracias kalema <graciasgray@gmail.com>
2018-09-21 00:16:44 +0000

    inital commit

---
+++ week_one/ruby_basics.rb
@@ -0,0 +1,24 @@
+# answer each of the following to the best of your ability
+# there is always more than one way to code a response 😄
+# please use your knowledge of variable assignments in each response
+# when you are done, share your code with your instructor, push it to Github, or submit a pull request
+
+
+# output an integer
+puts 5*3[4-2]-+4
+puts 4+3
+# output the number of seconds in a week
+
+puts 60
+puts 60*60
+puts 24*3600
+puts 86400*168
+# output a float
+
+puts 5/3.to_f
+puts 173/13.to_f
+# output a string
+puts"i like " + "playing games"
+
+
+# output concatination of strings
